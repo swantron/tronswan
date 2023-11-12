d21cbad6586fb6bdf8d0a018af5d922f528ac931
swantron <joe@swantron.com>
2023-11-12 14:28:39 -0700

    fix test

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          tron swan dot com
+          a react joint
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://chomptron.com"
           target="_blank"
           rel="noopener noreferrer"
         >
