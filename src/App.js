bbcb0465b3a3376bd496f823ef4049ee8c6c685d
swantron <joe@swantron.com>
2024-01-23 19:26:23 -0700

    weather

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
+import React, { useState, useEffect } from 'react';
 import logo from './robotard-removebg-preview.png';
 import './App.css';
 
 function App() {
+  const [temperature, setTemperature] = useState(null);
+
+  useEffect(() => {
+    const fetchTemperature = async () => {
+      const apiKey = 'WEATHER_API_KEY'; // DO variable
+      const city = 'Bozeman';
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        setTemperature(data.main.temp);
+      } catch (error) {
+        console.error('Error fetching temperature:', error);
+      }
+    };
+
+    fetchTemperature();
+  }, []);
+
   return (
-    <div className="App" data-testid="app-container">
-      <header className="App-header" data-testid="app-header">
-        <img src={logo} className="App-logo" alt="logo" data-testid="app-logo" />
-        <p data-testid="app-description">
-          tron swan dot com
-          a react joint
-        </p>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>tron swan dot com a react joint</p>
+        {temperature && <p>Current Temperature in Bozeman, MT: {temperature}°F</p>}
         <a
           className="App-link"
           href="https://swantron.com"
           target="_blank"
           rel="noopener noreferrer"
-          data-testid="swantron-link"
         >
           swan tron dot com
         </a>
@@ -24,7 +42,6 @@ function App() {
           href="https://chomptron.com"
           target="_blank"
           rel="noopener noreferrer"
-          data-testid="chomptron-link"
         >
           chomp tron dot com
         </a>
