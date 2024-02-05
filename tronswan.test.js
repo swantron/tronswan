eb30d105a7f475b991fd239c68c936585a9ac1a7
Joseph Swanson <jswanson@demandbase.com>
2024-02-05 13:22:22 -0700

    deployed

--- tronswan.test.js
+++ tronswan.test.js
@@ -3,8 +3,8 @@ const { test, expect } = require('@playwright/test');
 test.describe('TronSwan Website Tests', () => {
   test('Homepage should load and contain the swantron.com link', async ({ page }) => {
     // Navigate to your website
-    // await page.goto('https://tronswan.com');
-    await page.goto('localhost:3000');
+    await page.goto('https://tronswan.com');
+    // await page.goto('localhost:3000');
 
     // Check if the specific link element is visible
     await expect(page.locator('a[data-testid="swantron-link"]')).toBeVisible();
