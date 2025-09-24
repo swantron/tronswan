623a3970a594ad0c4bbbceb0e1102d3affb4e36d
swantron <joe@swantron.com>
2025-09-24 08:56:15 -0600

    consolidate css

--- src/components/video/Hacking.tsx
+++ src/components/video/Hacking.tsx
@@ -1,36 +1,13 @@
 import React from 'react';
-import '../../styles/Hacking.css';
+import VideoPage from './VideoPage';
 
 const Hacking = () => {
   return (
-    <div className='hacking-container' data-testid='hacking-container'>
-      <div className='hacking-content'>
-        <h1 className='hacking-title'>computer hacking skills</h1>
-
-        <div className='video-container'>
-          <video
-            autoPlay
-            muted
-            loop
-            playsInline
-            className='hacking-video'
-            data-testid='hacking-video'
-          >
-            {/* Prioritize web-optimized formats for better performance */}
-            <source src='/hacking.webm' type='video/webm' />
-            <source src='/hacking.mp4' type='video/mp4' />
-            <source src='/hacking.mov' type='video/quicktime' />
-            <p className='video-fallback'>
-              Your browser does not support the video tag.
-              <br />
-              <a href='/hacking.mp4' download>
-                Download MP4 version
-              </a>
-            </p>
-          </video>
-        </div>
-      </div>
-    </div>
+    <VideoPage
+      title="computer hacking skills"
+      videoSrc="/hacking.mp4"
+      testId="hacking"
+    />
   );
 };
 
--- src/components/video/VideoPage.tsx
+++ src/components/video/VideoPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../styles/Gangnam.css';
+import '../../styles/Video.css';
 
 interface VideoPageProps {
   title: string;
