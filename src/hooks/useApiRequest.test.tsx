6e80832907bd842f294120ebae4fe029dd8377c1
swantron <joe@swantron.com>
2025-10-06 21:37:34 -0600

    bump react testing lib

--- src/hooks/useApiRequest.test.tsx
+++ src/hooks/useApiRequest.test.tsx
@@ -1,4 +1,5 @@
-import { renderHook, act, waitFor } from '@testing-library/react';
+import { renderHook, waitFor } from '@testing-library/react';
+import { act } from 'react';
 import {
   vi,
   expect,
