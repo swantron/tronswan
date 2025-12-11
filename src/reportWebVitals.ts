2c78972499ca04110568ab67db8127b197391eb6
swantron <joe@swantron.com>
2025-12-10 20:51:03 -0700

    react 19 migration

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -2,12 +2,12 @@ type PerfEntryCallback = (entry: any) => void;
 
 const reportWebVitals = (onPerfEntry?: PerfEntryCallback) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
+    import('web-vitals').then(({ onCLS, onINP, onFCP, onLCP, onTTFB }) => {
+      onCLS(onPerfEntry);
+      onINP(onPerfEntry); // INP replaces FID in v5
+      onFCP(onPerfEntry);
+      onLCP(onPerfEntry);
+      onTTFB(onPerfEntry);
     });
   }
 };
